ad7a1d67930bf986f4d3f30f35e74133026c620a
Frank LPC <gowreeswaran.sampanther@gmail.com>
2022-12-15 18:48:48 +0530

    Suceesfully Created Enqueue

--- queue.py
+++ queue.py
@@ -16,14 +16,29 @@
 class CircularQueue():
     # its a initializer aka constrcutor
     def __init__(self, k):
-        print('initiated with size ', str(k))
+        #print('initiated with size ', str(k))
         self.k = k
         self.queue = [None] * k
-        self.front = self.rear = -1
+        self.head = self.tail = -1
+
+    def enqueue(self, data):
+
+        if ((self.tail + 1) % self.k == self.head):
+            print("The circular queue is full\n")
+
+        elif (self.head == -1):
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
+        else:
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+
     
 
 
 testCircularQueue = CircularQueue(3)
+
 
 
 
