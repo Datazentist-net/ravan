18d3f5b0997f7765f148cf980b39aafbb0d36ca8
Frank LPC <gowreeswaran.sampanther@gmail.com>
2022-12-23 16:28:59 +0530

    updated

---
+++ linked-list.py
@@ -0,0 +1,11 @@
+class Node:
+    # Creating a node
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
