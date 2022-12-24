47589b011eefc3c54c9a5c0fcb45af270085725b
Frank LPC <gowreeswaran.sampanther@gmail.com>
2022-12-24 21:07:51 +0530

    updated

--- hashtable.py
+++ hashtable.py
@@ -4,19 +4,48 @@
 #  
 s = "b" # 65 66 ----- 97 
 st = "INFORMIX"
+key_list = []
 
 value1 = "cat" # 312 Indrajith
 value2 = "dog" # 314 Pallavi
 value3 = "act" # 312 Kajan
 
 
+# define the hash function to generate the hashed value for a key
+
+def hash(value):
+    ascii_result = 0
+    for ch in value:
+        ascii_result += ord(ch)
+    return ascii_result
+
+# Please note take the array length as 4 for this sample only
+# Get the value, genearate the key from returning ascii value and using array length 
+
+#def key_value_place(value):
+
+
+
+print(hash(value1))
+print(hash(value2))
+print(hash(value3))
+
+
+
+
+
 # Indrajith
-sumValue1 = 0
+sumValue1 = 0 # initialise 
+# 1st time = 99
+ #      sumvalue = 0 + 99
+#2nd time
+# sumvalue = 196
+# 3rd Time
+# 196 + 116 = 312
+
 for charStr in value1:
-    sumValue1 = sumValue1 + ord(charStr)
+    sumValue1  +=  ord(charStr)
 print(value1 + "'s ASCII Value is " + str(sumValue1))
 
-for ch in value1:
-    print (ch)
 
 # Repetition take the one char from the string and get the ascii value sum it until reach the last char
