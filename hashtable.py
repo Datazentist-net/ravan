2451013efd43a7f5d760293f047ab74974c987ce
Frank LPC <gowreeswaran.sampanther@gmail.com>
2022-12-24 20:58:32 +0530

    updated

--- hashtable.py
+++ hashtable.py
@@ -4,34 +4,10 @@
 #  
 s = "b" # 65 66 ----- 97 
 st = "INFORMIX"
-key_list = []
 
 value1 = "cat" # 312 Indrajith
 value2 = "dog" # 314 Pallavi
 value3 = "act" # 312 Kajan
-
-
-# define the hash function to generate the hashed value for a key
-
-def hash(value):
-    ascii_result = 0
-    for ch in value:
-        ascii_result += ord(ch)
-    return ascii_result
-
-# Please note take the array length as 4 for this sample only
-# Get the value, genearate the key from returning ascii value and using array length 
-
-#def key_value_place(value):
-
-
-
-print(hash(value1))
-print(hash(value2))
-print(hash(value3))
-
-
-
 
 
 # Indrajith
